eac9f66a7f848d209493152ab46cd69353920740
Val Patterson <val@nutznboltz.website>
2021-10-10 22:50:24 -0400

    lots of changes to bring code even with demonstration

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log('success');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function() {
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+    } else {
+        header.classList.add('open');
+    }
+});

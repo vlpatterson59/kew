c11236f8e0ab33ec49d0b10bbeacd1d6e9b8b4af
Val Patterson <val@nutznboltz.website>
2022-05-19 22:45:38 -0400

    captcha debugged

--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -15,30 +15,49 @@ $subject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", 
 // $website = isset( $_POST['website'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['website'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
-$mail_subject = 'A contact request sent by ' . $name;
+$captcha;
 
-$body = 'Name: '. $name . "\r\n";
-$body .= 'Email: '. $senderEmail . "\r\n";
+if(isset($_POST['g-recaptcha-response'])){
+  $captcha=$_POST['g-recaptcha-response'];
+}
 
+if(!$captcha) {
+  echo 'Please check the captcha form';
+  exit;
+}
 
-if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
-// if ($services) {$body .= 'services: '. $services . "\r\n"; }
-if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
-// if ($address) {$body .= 'Address: '. $address . "\r\n"; }
-// if ($website) {$body .= 'Website: '. $website . "\r\n"; }
+$secretKey = "6LdVtAMgAAAAAN5tEg0AB-I4wDUeWIYdfB-ApA4C";
+$ip = $_SERVER['REMOTE_ADDR'];
+$url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) . '&response=' . urlencode($captcha);
+$response = file_get_contents($url);
+$responseKeys = json_decode($response, true);
 
-$body .= 'message: ' . "\r\n" . $message;
+if($responseKeys["success"]) {
+  $mail_subject = 'A contact request sent by ' . $name;
 
+  $body = 'Name: '. $name . "\r\n";
+  $body .= 'Email: '. $senderEmail . "\r\n";
 
+  if ($phone) {$body .= 'Phone: '. $phone . "\r\n"; }
+  // if ($services) {$body .= 'services: '. $services . "\r\n"; }
+  if ($subject) {$body .= 'Subject: '. $subject . "\r\n"; }
+  // if ($address) {$body .= 'Address: '. $address . "\r\n"; }
+  // if ($website) {$body .= 'Website: '. $website . "\r\n"; }
 
-// If all values exist, send the email
-if ( $name && $senderEmail && $message ) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $name . " <" . $senderEmail . ">";  
-  $success = mail( $recipient, $mail_subject, $body, $headers );
-  echo "<div class='inner success'><p class='success'>Thanks for contacting us. We will contact you ASAP!</p></div><!-- /.inner -->";
-}else {
-	echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
+  $body .= 'message: ' . "\r\n" . $message;
+
+  // If all values exist, send the email
+  if ( $name && $senderEmail && $message ) {
+    $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+    $headers = "From: " . $name . " <" . $senderEmail . ">";  
+    $success = mail( $recipient, $mail_subject, $body, $headers );
+    echo "<div class='inner success'><p class='success'>Thanks for contacting us. We will contact you ASAP!</p></div><!-- /.inner -->";
+  }else {
+    echo "<div class='inner error'><p class='error'>Something went wrong. Please try again.</p></div><!-- /.inner -->";
+  }
+} else {
+  echo 'Captcha verification failed';
+  exit;
 }
 
 ?>
